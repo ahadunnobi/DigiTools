65232335e79956809d2cd52cdff371207aea2517
ahadunnobi <hacker.ven10@gmail.com>
2026-04-01 19:27:15 +0600

    data created3

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Stats from './components/Stats';
+import ProductCard from './components/Products';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <main>
         <Hero />
         <Stats />
+        <Products />
       </main>
       <ToastContainer />
     </div>
